bd74a63775773bd605f10f00c45fbd5b54c057cb
BrenoSB98 <brenosilvabraido1998@gmail.com>
2022-12-26 21:48:34 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao{
+
+	@Override
+	public void inserir(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletaPorId(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor localizaPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> localizaTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
